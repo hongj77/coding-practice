100cea774963010714b93ca1f320eb49829005c5
Hong <hongj77@gmail.com>
2015-08-05 23:47:06 -0400

    input file testing

--- codeforces/567/A_567.cpp
+++ codeforces/567/A_567.cpp
@@ -13,28 +13,28 @@ int n;
 int c[100050];
 
 int main() {
-	cin >> n;
-	for (int i = 0; i < n; ++i) {
-		cin >> c[i];
-	}
-	
-	for (int i = 0; i < n; ++i) {
-		int mx, mn, l, r;
-		
-		r = c[n-1] - c[i];
-		l = c[i] - c[0];
-		mx = max(l,r);
-		
-		if (i == 0) {
-			mn = c[1] - c[0];
-		}
-		else if (i == n-1) {
-			mn = c[n-1] - c[n-2];
-		}
-		else {
-		  mn = min(c[i+1]-c[i], c[i]-c[i-1]);
-		}
-		cout << mn << " " << mx << "\n";
-	}
-	return 0;
+  cin >> n;
+  for (int i = 0; i < n; ++i) {
+    cin >> c[i];
+  }
+    
+  for (int i = 0; i < n; ++i) {
+    int mx, mn, l, r;
+        
+    r = c[n-1] - c[i];
+    l = c[i] - c[0];
+    mx = max(l,r);
+        
+    if (i == 0) {
+      mn = c[1] - c[0];
+    }
+    else if (i == n-1) {
+      mn = c[n-1] - c[n-2];
+    }
+    else {
+      mn = min(c[i+1]-c[i], c[i]-c[i-1]);
+    }
+      cout << mn << " " << mx << "\n";
+    }
+    return 0;
 }
