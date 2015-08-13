f9183ce892ed0d2f130b0b417074b4ffce0aab06
Hong <hongj77@gmail.com>
2015-08-12 22:43:32 -0400

    lowest common ancestor tree

---
+++ interview/lowestCA.cpp
@@ -0,0 +1,61 @@
+/* Let T be a rooted tree. The lowest common ancestor between two nodes n1 and n2 is defined as the lowest node in T that has both n1 and n2 as descendants (where we allow a node to be a descendant of itself). */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Node {
+  int key;
+  struct Node *left, *right;
+};
+
+Node* newNode(int k) {
+  Node *temp = new Node;
+  temp->key = k;
+  temp->left = NULL;
+  temp->right = NULL;
+  return temp;
+}
+
+// must assume that n1 and n2 are both in the tree
+int findLCA(Node* root, int n1, int n2) {
+  if (root == NULL) {
+    return -1;
+  }
+  if (root->key == n1 || root->key == n2) {
+    return root->key;
+  }
+  int left = findLCA(root->left, n1, n2);
+  int right = findLCA(root->right, n1,n2); 
+
+  if (left != -1 && right != -1) {
+    return root->key;
+  }
+  if (left != -1) {
+    return left;
+  }
+  if (right != -1) {
+    return right;
+  }
+  return -1;
+}
+
+int main() {
+  Node* root = newNode(1);
+  root->left = newNode(2);
+  root->right = newNode(3);
+  root->left->left = newNode(4);
+  root->left->right = newNode(5);
+  root->right->left = newNode(6); 
+  root->right->right = newNode(7);
+
+  cout<<findLCA(root,4,5) << endl;
+  cout<<findLCA(root,3,6) << endl;
+  cout<<findLCA(root,7,5) << endl;
+
+
+  return 0;
+}
+
+
