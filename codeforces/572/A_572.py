06ace193db1978c39681ed9a1de7b89bd4ff5708
Hong <hongj77@gmail.com>
2015-08-27 21:02:32 -0400

    codeforces 572A python practice

---
+++ codeforces/572/A_572.py
@@ -0,0 +1,14 @@
+
+
+if __name__=="__main__":
+  size_a, size_b = map(int, raw_input().split()) 
+  k, m = map(int, raw_input().split())
+  A = list(map(int, raw_input().split()))
+  B = list(map(int, raw_input().split()))
+
+  if A[k-1] < B[size_b-m]:
+   print "YES"
+  else:
+    print "NO"
+
+
