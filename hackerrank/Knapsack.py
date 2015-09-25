ef9436b973c9155dd5e681f7917d5b37dd879444
Hong <hongj77@gmail.com>
2015-09-25 15:19:39 -0400

    broadway interview

---
+++ hackerrank/Knapsack.py
@@ -0,0 +1,13 @@
+k = int(raw_input())
+ls = map(int, raw_input().split())
+
+
+''' sol[i-1] is the number closest to k using i-1 first elements 
+	k = 12
+	ls = [1,3,6]
+	{6,6,6} makes 12
+'''
+
+
+
+
---
+++ interview/random/palin.py
@@ -0,0 +1,28 @@
+
+
+word = raw_input()
+
+def isPalin(word):
+  for i in range(len(word)/2):
+    if word[i] != word[-1-i]:
+      return False
+  return True
+
+
+''' how many letters do you have to add to the RIGHT side of the word
+    to make it a palindrome. Find the minimum count. '''
+def palinAddition(word):
+  letters = 0
+  begin = 0
+  end = len(word)-1
+
+  while begin < end:
+    if isPalin(word[begin:end+1]):
+      break
+    else:
+      letters += 1
+      begin += 1
+  return letters
+     
+
+print palinAddition(word)
